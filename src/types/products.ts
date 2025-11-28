c22fb8933b97e8320dc5d1dfc4a550907e7e78f3
Emanuele Favero <emafaux89@gmail.com>
2025-11-28 19:34:16 +0100

    feat: add product status field in product type

--- src/types/products.ts
+++ src/types/products.ts
@@ -3,10 +3,16 @@ export type Product = {
   name: string
   quantity: number
   category?: string
+  status: ProductStatus
 }
 
-export type NewProduct = Omit<Product, 'id'>
+export type NewProduct = Omit<Product, 'id' | 'status'>
 export type ProductId = Product['id']
 export type ProductQuantity = Product['quantity']
 export type ProductQuantityOperation = 'increment' | 'decrement'
+export type ProductStatus =
+  | 'IN_STOCK'
+  | 'CHECKED_OUT'
+  | 'MAINTENANCE'
+  | 'DISCONTINUED'
 export type Products = Product[]
