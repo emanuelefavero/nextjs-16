a82969cda2dad7db679604a5de5d0e6ad253af9f
Emanuele Favero <emafaux89@gmail.com>
2025-12-17 16:32:23 +0100

    feat: add BackButton improvements

--- src/components/shared/back-button.tsx
+++ src/components/shared/back-button.tsx
@@ -1,11 +1,12 @@
 'use client'
 
+import { cn } from '@/lib/utils'
 import { usePathname, useRouter } from 'next/navigation'
 import { useEffect, useEffectEvent, useState } from 'react'
 
 type Props = Omit<React.ComponentProps<'button'>, 'onClick' | 'disabled'>
 
-export function BackButton({ children, ...props }: Props) {
+export function BackButton({ children, className, ...props }: Props) {
   const router = useRouter()
   const pathname = usePathname()
   const [canGoBack, setCanGoBack] = useState(false)
@@ -15,14 +16,17 @@ export function BackButton({ children, ...props }: Props) {
   })
 
   useEffect(() => {
+    if (typeof window === 'undefined') return
     checkHistoryLength()
   }, [pathname])
 
   return (
     <button
       type='button'
+      className={cn('', className)}
       onClick={() => router.back()}
       disabled={!canGoBack}
+      aria-label={typeof children === 'string' ? children : 'Go back'}
       {...props}
     >
       {children ?? 'Back'}
